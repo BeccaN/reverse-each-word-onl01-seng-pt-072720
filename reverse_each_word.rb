ff7685bbd5ed3a1da5491cd4dd72c0d11fdef79b
Rebecca Congi <rncongi@gmail.com>
2020-08-03 20:38:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
-  string.split.collect do |word|
-    word.reverse
+  string.split.map {|word| word.reverse.join(" ")
+
   end
 end 
 
+sentence.split.map {|word| word.reverse}.join(" ")
